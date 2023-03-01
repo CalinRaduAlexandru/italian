5dc2fd1ea4f995804159c84836252037bf996d67
Calin <calinradualexandru@protonmail.com>
2023-03-01 12:22:07 +0200

    Mobile first

--- main.py
+++ main.py
@@ -3,35 +3,33 @@ import openai
 import os
 from dotenv import load_dotenv
 
+
 def configure():
     load_dotenv()
+
 
 openai.api_key = os.getenv('api_key')
 
 app = Flask(__name__)
 
+
 # @app.route('/', methods=['GET'])
 def create_prompt(user_input=""):
-    prompt = f"""
-    Per favore, fai la correzione e la traduzione in rumeno per il mio input e doppo che mi hai risposto, fai anche la traduzione per la tua risposta. Fa essatamente come negli esempi e non dimenticare di continuare la conversazione:
-Esempio:
-[io]: Sei fantastico! Adoro imparare così con te!
-[italiano]: Il tuo testo corretto: "Sei fantastico! Mi piace imparare così con te!"
-Traducerea în română: "Ești fantastic! Îmi place să învăț așa cu tine!"
-[italiano]: Grazie per i tuoi commenti! Sono felice di sapere che ti stai godendo la nostra sessione di apprendimento dello spagnolo. C'è qualcosa in particolare che vuoi chiedere o praticare oggi?
-Traduzione rumena: “Mulțumesc pentru comentarii! Mă bucur să știu că vă bucurați de sesiunea noastră de învățare a spaniolei. Vrei să întrebi sau să exersezi ceva anume astăzi?"
-Inizia correggendo questo testo e rispondi: {user_input}
-#     """
-#     prompt = f"""Exemplu de input: 'Sneakers Nike Jordan 1 Size(42.5)'
-#     Outputul pe care il astept de la tine este o descriere de prdous de 150 de cuvinte ca in exemplu de mai jos, incepe cu titlul:
-#     ### Titlul produsului ###
-#     Despre produsul Sneakers Nike Jordan 1 Size(42.5):\n
-#     ### Descrere produs ###
-#     Sneakers Nike Jordan 1 Size(42.5) sunt o alegere perfectă pentru cei care doresc să își completeze colecția cu o pereche de adidași de calitate superioară. Designul acestor sneakers este atemporal. Combinația de culori atrăgătoare fac din aceștia o alegere ideală atât pentru purtarea zilnică, cât și pentru ocazii speciale. Materialele de calitate premium ale Sneakers Nike Jordan 1 Size(42.5) asigură un confort maxim, iar talpa cu profil înalt oferă o aderență bună. Brandul Nike și logo-ul Jordan sunt simboluri recunoscute la nivel mondial și aduc un plus de stil și eleganță. Nu ratați ocazia de a vă procura acești sneakers unici si comozi!\n
-#     ### Descrere META ###
-#     Descriere meta de maxim 80 de caractere: Nike Jordan 1 Size(42.5) sunt ideali pentru pasionații de adidași de calitate superioară. Designul atemporal și culorile atrăgătoare fac diferența.
-#     Pe exact structura asta, fa-mi o descriere pentru: {user_input}
-#     """
+    prompt = f""" Per favore, usando al minimo 200 parole, fai la correzione e la traduzione in rumeno per il mio input e fai anche la traduzione per la tua risposta. Fa essatamente come negli esempi e non dimenticare di continuare la conversazione:
+Il tuo testo corretto: "Voglio imparare l'italiano ma non so come fare." Traducerea în română: "Vreau să învăț italiana dar nu știu cum să fac asta.". Risponso: Capisco! Ci sono molte risorse online che puoi usare per imparare l'italiano. Quale tipo di aiuto cercavi? Raspuns: Am înțeles! Există multe resurse online pe care le puteți folosi pentru a învăța limba italiană. Ce fel de ajutor căutai?
+Inizia correggendo questo testo e rispondi:
+     {user_input}
+    """
+    # prompt = f"""Exemplu de input: 'Sneakers Nike Jordan 1 Size(42.5)'
+    # Outputul pe care il astept de la tine este o descriere de prdous de 150 de cuvinte ca in exemplu de mai jos, incepe cu titlul:
+    # ### Titlul produsului ###
+    # Despre produsul Sneakers Nike Jordan 1 Size(42.5):\n
+    # ### Descrere produs ###
+    # Sneakers Nike Jordan 1 Size(42.5) sunt o alegere perfectă pentru cei care doresc să își completeze colecția cu o pereche de adidași de calitate superioară. Designul acestor sneakers este atemporal. Combinația de culori atrăgătoare fac din aceștia o alegere ideală atât pentru purtarea zilnică, cât și pentru ocazii speciale. Materialele de calitate premium ale Sneakers Nike Jordan 1 Size(42.5) asigură un confort maxim, iar talpa cu profil înalt oferă o aderență bună. Brandul Nike și logo-ul Jordan sunt simboluri recunoscute la nivel mondial și aduc un plus de stil și eleganță. Nu ratați ocazia de a vă procura acești sneakers unici si comozi!\n
+    # ### Descrere META ###
+    # Descriere meta de maxim 80 de caractere: Nike Jordan 1 Size(42.5) sunt ideali pentru pasionații de adidași de calitate superioară. Designul atemporal și culorile atrăgătoare fac diferența.
+    # Pe exact structura asta, fa-mi o descriere pentru: {user_input}
+    # """
     return prompt
 
 
@@ -39,7 +37,7 @@ def discussion(prompt):
     data = openai.Completion.create(
         model="text-davinci-003",
         prompt=prompt,
-        temperature=0.3,
+        temperature=0.7,
         max_tokens=1000,
         top_p=1.0,
         frequency_penalty=0.0,
@@ -50,6 +48,7 @@ def discussion(prompt):
     print(data)
 
     return new_data
+
 
 @app.route('/andrea', methods=['GET', 'POST'])
 def chat():
@@ -68,5 +67,4 @@ def get_chat():
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
-
+    app.run(debug=True)
